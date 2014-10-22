1d207138c484dca395d3e90e62affb94767b2e6e
karthikkpati@gmail.com <karthikkpati@gmail.com@57266b51-173f-c787-d3c6-40d8d4b77488>
2014-10-22 14:02:32 +0000

    Adding delete event api

---
+++ trunk/src/main/java/com/solweaver/greetings/dto/EventDeleteRequest.java
@@ -0,0 +1,36 @@
+package com.solweaver.greetings.dto;
+
+import javax.validation.constraints.NotNull;
+
+
+public class EventDeleteRequest extends BaseRequest{
+
+	@NotNull(message="UserId cannot be null")
+	private Long userId;
+	
+
+	@NotNull(message="EventId cannot be null")
+	private Long eventId;
+
+
+	public Long getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+
+	public Long getEventId() {
+		return eventId;
+	}
+
+
+	public void setEventId(Long eventId) {
+		this.eventId = eventId;
+	}
+
+	
+}
